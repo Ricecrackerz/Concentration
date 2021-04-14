2af24a3e21325c95bea7ab7a809002ef3c9a3173
Vincent H <hoangvincent69@gmail.com>
2021-04-14 15:06:40 -0700

    Added buttons and activities

--- app/src/main/java/com/example/concentration/MainActivity.java
+++ app/src/main/java/com/example/concentration/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.concentration;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnGame;
+    private Button btnMusic;
+    private Button btnHs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnGame = findViewById(R.id.btnGame);
+        btnMusic = findViewById(R.id.btnMusic);
+        btnHs = findViewById(R.id.btnHs);
+
+        btnGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goGameActivity();
+            }
+        });
+
+        btnHs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goHighScoreActivity();
+            }
+        });
+
+    }
+    private void goGameActivity(){
+        Intent i = new Intent(this, GameActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    private void goHighScoreActivity(){
+        Intent i = new Intent(this, HighScoreActivity.class);
+        startActivity(i);
+        finish();
     }
 }
