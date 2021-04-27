f4a734dfcfc1c439bb4d66feb00976c511e7c28b
nguyentlan04@gmail.com <nguyentlan04@gmail.com>
2021-04-26 19:50:51 -0700

    Fnished Game Logic. Need to implement last two buttons and different cards.

--- app/src/main/java/com/example/concentration/GameActivity.java
+++ app/src/main/java/com/example/concentration/GameActivity.java
@@ -1,26 +1,24 @@
 package com.example.concentration;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.Image;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
-import static com.example.concentration.R.id.btn1;
-import static com.example.concentration.R.id.text;
 
 public class GameActivity extends AppCompatActivity {
 
     Button btnTryAgain;
     public static int clicks = 0;
-    public static int lastClicked = -1, lastClicked1 = -1;
+    public static int firstClicked = -1, lastClicked = -1;
+    public static int firstImage = 0, secondImage = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,63 +48,96 @@ public class GameActivity extends AppCompatActivity {
         wordList.add("PLATYPUS");
         wordList.add("GOAT");
 
-        // If we want to attempt images
-//        ArrayList<Image> image = new ArrayList<>();
-//        Button button = (Button) findViewById(R.id.button);
-//        Image tony = getImage(getDocumentBase(), );
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                button.setBackgroundResource(animals[0]);
-//            }
-//        });
+        ArrayList<Integer> images = new ArrayList<>();
 
-        int [] idArray = {R.id.btn1, R.id.btn2, R.id.btn3};
-        int [] idTextView = {R.id.tv1, R.id.tv2, R.id.tv3};
+        images.add(R.drawable.tinykirby);
+        images.add(R.drawable.tinykirby);
+        images.add(R.drawable.tony);
+        images.add(R.drawable.tony);
+        /*images.add(R.drawable.tinykirby);
+        images.add(R.drawable.tinykirby);
+        images.add(R.drawable.tinykirby);
+        images.add(R.drawable.tinykirby);
+        images.add(R.drawable.tinykirby);
+        images.add(R.drawable.tinykirby);
+        images.add(R.drawable.tinykirby);
+        images.add(R.drawable.tinykirby);
+        images.add(R.drawable.tinykirby);
+        images.add(R.drawable.tinykirby);
+        images.add(R.drawable.tinykirby);
+        images.add(R.drawable.tinykirby);
+        images.add(R.drawable.tinykirby);
+        images.add(R.drawable.tinykirby);
+        images.add(R.drawable.tinykirby);
+        images.add(R.drawable.tinykirby);
+        images.add(R.drawable.tinykirby);
+        images.add(R.drawable.tinykirby);
+        images.add(R.drawable.tony);
+        images.add(R.drawable.tony);*/
 
+        int cardBack = R.drawable.cardback;
 
-        TextView textView[] = new TextView[idTextView.length];
-        Button buttons[] = new Button[idArray.length];
-        Collections.shuffle(wordList);
+        int [] idButton = {R.id.btn1, R.id.btn2,R.id.btn3,R.id.btn4,R.id.btn5,R.id.btn6,R.id.btn7,R.id.btn8, R.id.btn9,
+                R.id.btn10,R.id.btn11,R.id.btn12,R.id.btn13,R.id.btn14,R.id.btn15,R.id.btn16,R.id.btn17,R.id.btn18,
+                R.id.btn19,R.id.btn20};
+
+        Button buttons[] = new Button[idButton.length];
+
+        Collections.shuffle(images);
 
         btnTryAgain = (Button) findViewById(R.id.btnTryAgain);
 
-        for(int i = 0; i < idArray.length; i++){
+        for(int i = 0; i < idButton.length; i++){
 
-            buttons[i] = (Button) findViewById(idArray[i]);
-            textView[i] = (TextView) findViewById(idTextView[i]);
-            buttons[i].setText("cardBack");
+            buttons[i] = (Button) findViewById(idButton[i]);
+            buttons[i].setCompoundDrawablesWithIntrinsicBounds(cardBack, 0, 0, 0);
             int finalI = i;
 
 
             buttons[i].setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if(buttons[finalI].getText() == "cardBack" && clicks < 2)
-                    {
-                        buttons[finalI].setVisibility(View.GONE);
-                        textView[finalI].setText(wordList.get(finalI));
-                        buttons[finalI].setText("ok");
-                        if(clicks == 0){
+                    if(buttons[finalI].isClickable() == true && clicks < 2) {
+                        buttons[finalI].setCompoundDrawablesWithIntrinsicBounds(images.get(finalI), 0, 0, 0);
+                        buttons[finalI].setClickable(false);
+
+                        if (clicks == 0) {
+                            firstImage = images.get(finalI);
+                            firstClicked = finalI;
+                            System.out.println(firstImage);
+                        } else if (clicks == 1) {
+                            secondImage = images.get(finalI);
                             lastClicked = finalI;
-                        }
-                        else if(clicks == 1){
-                            lastClicked1 = finalI;
+                            System.out.println(secondImage);
                         }
                         clicks++;
+                    }
+                    if (clicks == 2) {
+                         if (firstImage != secondImage){
+                            System.out.println(":(");
+                            buttons[firstClicked].setClickable(false);
+                            buttons[lastClicked].setClickable(false);
+                        }
+                        else if (firstImage == secondImage){
+                            System.out.println("lets");
+                            buttons[firstClicked].setClickable(false);
+                            buttons[lastClicked].setClickable(false);
+                            clicks = 0;
+                            firstImage = 0;
+                            secondImage = 0;
+                        }
                     }
                 }
             });
 
-            // Need to fix this
             btnTryAgain.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     if(clicks == 2){
-                        buttons[lastClicked].setVisibility(View.VISIBLE);
-                        buttons[lastClicked1].setVisibility(View.VISIBLE);
-                        buttons[lastClicked].setText("cardBack");
-                        buttons[lastClicked1].setText("cardBack");
+                        buttons[firstClicked].setCompoundDrawablesWithIntrinsicBounds(cardBack, 0, 0, 0);
+                        buttons[lastClicked].setCompoundDrawablesWithIntrinsicBounds(cardBack, 0, 0, 0);
+                        buttons[firstClicked].setClickable(true);
+                        buttons[lastClicked].setClickable(true);
                         clicks = 0;
                     }
                 }
